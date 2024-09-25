2d80516836bf633e883fc9f595c5fec49e2e07db
Thanh Duy <duydtrai1@gmail.com>
2024-09-25 19:10:30 +0700

    Add files via upload

---
+++ sosanhtuoi2nguoi/bin/sosanhtuoi2nguoi.dart
@@ -0,0 +1,11 @@
+void main() {
+  var agePerson1 = 18;
+  var agePerson2 = 20;
+  if (agePerson1 > agePerson2) {
+    print("Tuoi nguoi 1 lon hon tuoi nguoi 2");
+  } else if (agePerson1 == agePerson2) {
+    print("Hai nguoi bang tuoi");
+  } else {
+    print('nguoi 2 lon tuoi hon nguoi 1');
+  }
+}
---
+++ sosanhtuoi2nguoi/test/sosanhtuoi2nguoi_test.dart
@@ -0,0 +1,8 @@
+import 'package:sosanhtuoi2nguoi/sosanhtuoi2nguoi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
